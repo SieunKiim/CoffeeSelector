5a272e3fdd3f03d799ec3dc35607941d0e902c23
SieunKiim <sieun960513@naver.com>
2024-04-07 19:57:16 +0900

    Updates

---
+++ precache-manifest.0f560c41ec5b9a608b5e194fe0e8c7de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77802b0ab27a8bfe3bd4ca176c18cf1",
+    "url": "/CoffeeSelector/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/CoffeeSelector/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/CoffeeSelector/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/CoffeeSelector/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/CoffeeSelector/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "e6bd365614ba3afd2046",
+    "url": "/CoffeeSelector/static/js/runtime~main.5d0f846a.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/CoffeeSelector/static/media/logo.06e73328.svg"
+  }
+]);
